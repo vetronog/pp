2c6e518ee8044f19df5adc42aed6b77b863cb024
Hollow <Hollow@HOLLOW->
2018-06-10 18:59:58 +0300

    Однопоточная рабочая версия программы

--- Lab1/Lab1/Algorithm.cpp
+++ Lab1/Lab1/Algorithm.cpp
@@ -1,12 +1,43 @@
 #include "stdafx.h"
 #include "Algorithm.h"
+#include <cstdlib>
+static const size_t PI_COEFICIENT = 4;
+static const size_t EXPONENT = 2;
+static const size_t CIRCLE_RADIUS = 1;
+static const size_t DIAMETER = 2 * CIRCLE_RADIUS;
 
+size_t Algorithm::m_pointsInCircleCount = 0;
 
-Algorithm::Algorithm()
+Algorithm::Algorithm(size_t iterCount, size_t thrCount)
+	: m_iterationsCount(iterCount)
+	, m_threadCount(thrCount)
 {
 }
 
-
-Algorithm::~Algorithm()
+double Algorithm::GetPiNumber() const
 {
+	return m_pi;
+}
+
+void Algorithm::Start()
+{
+	std::srand(time(0));
+	float x;
+	float y;
+	for (size_t i = 0; i < m_iterationsCount; ++i)
+	{
+		x = rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
+		y = rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
+		if (IsPointInCircle(x,y))
+		{
+			++m_pointsInCircleCount;
+		}
+	}
+
+	m_pi = PI_COEFICIENT * (double)m_pointsInCircleCount / m_iterationsCount;
+}
+
+bool Algorithm::IsPointInCircle(float x, float y)
+{
+	return (pow(x, EXPONENT) + pow(y, EXPONENT)) <= CIRCLE_RADIUS;
 }
--- Lab1/Lab1/Algorithm.h
+++ Lab1/Lab1/Algorithm.h
@@ -2,7 +2,15 @@
 class Algorithm
 {
 public:
-	Algorithm();
-	~Algorithm();
+	Algorithm(size_t iterCount, size_t thrCount);
+	~Algorithm() = default;
+	void Start();
+	double GetPiNumber() const;
+private:
+	static bool IsPointInCircle(float x, float y);
+	double m_pi;
+	static size_t m_pointsInCircleCount;
+	size_t m_iterationsCount; 
+	size_t m_threadCount;
 };
 
