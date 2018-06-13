8bd3684e0dfcd138dc0a8f2e15ad8ba3764a9779
Anton <vetronog@yandex.ru>
2018-06-13 10:59:08 +0300

    Закончена работа с многопоточным алгоритмом

--- Lab1/Lab1/Algorithm.cpp
+++ Lab1/Lab1/Algorithm.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Algorithm.h"
+#include "ThreadController.h"
 #include <cstdlib>
 static const size_t PI_COEFICIENT = 4;
 static const size_t EXPONENT = 2;
@@ -9,9 +10,9 @@ static const size_t DIAMETER = 2 * CIRCLE_RADIUS;
 size_t Algorithm::m_pointsInCircleCount = 0;
 
 Algorithm::Algorithm(size_t iterCount, size_t thrCount)
-	: m_iterationsCount(iterCount)
+	: m_iterationCount(iterCount)
 	, m_threadCount(thrCount)
-{
+{	std::srand(time(0));
 }
 
 double Algorithm::GetPiNumber() const
@@ -21,22 +22,77 @@ double Algorithm::GetPiNumber() const
 
 void Algorithm::Start()
 {
-	std::srand(time(0));
+	if (m_threadCount == 1)
+	{
+		SingleThreadAlgorithm();
+	}
+	else
+	{
+		MultiThreadAlgorithm();
+	}
+}
+
+void Algorithm::SingleThreadAlgorithm()
+{
+	
 	float x;
 	float y;
-	for (size_t i = 0; i < m_iterationsCount; ++i)
+	for (size_t i = 0; i < m_iterationCount; ++i)
 	{
 		x = (float)rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
-		y = float)rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
-		if (IsPointInCircle(x,y))
+		y = (float)rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
+		if (IsPointInCircle(x, y))
 		{
 			++m_pointsInCircleCount;
 		}
 	}
 
-	m_pi = PI_COEFICIENT * (double)m_pointsInCircleCount / m_iterationsCount;
+	 m_pi = PI_COEFICIENT * (double)m_pointsInCircleCount / m_iterationCount;
 }
 
+DWORD Algorithm::CalculatePointsInCircle(LPVOID data)
+{
+	float x;
+	float y;
+	size_t* iterationCount = static_cast<size_t*>(data);
+	for (size_t i = 0; i < *iterationCount; ++i)
+	{
+		x = (float)rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
+		y = (float)rand() / RAND_MAX * DIAMETER - CIRCLE_RADIUS;
+		if (IsPointInCircle(x, y))
+		{
+			InterlockedIncrement(&m_pointsInCircleCount);
+		}
+	}
+	std::printf("%d\n", m_pointsInCircleCount);
+	return 0;
+}
+
+void Algorithm::MultiThreadAlgorithm()
+{
+	ThreadController controller;
+	std::vector<size_t> iterationThreadCount;
+	size_t iterCount = m_iterationCount / m_threadCount;
+	size_t iterRest = m_iterationCount % m_threadCount;
+
+	for (size_t i = 0; i < m_threadCount; i++)
+	{
+		iterationThreadCount.emplace_back(iterCount);
+	}
+	for (size_t i = 0; i < iterRest; ++i)
+	{
+		++iterationThreadCount.at(i);
+	}
+	for (size_t i = 0; i < m_threadCount; ++i)
+	{
+		controller.Add(&CalculatePointsInCircle, &iterationThreadCount[i]);
+	}
+	controller.WaitAll();
+	std::printf("%d\n", m_pointsInCircleCount);
+
+	std::printf("%d\n", m_iterationCount);
+	m_pi = PI_COEFICIENT * (double)m_pointsInCircleCount / m_iterationCount;
+}
 bool Algorithm::IsPointInCircle(float x, float y)
 {
 	return (pow(x, EXPONENT) + pow(y, EXPONENT)) <= CIRCLE_RADIUS;
--- Lab1/Lab1/Algorithm.h
+++ Lab1/Lab1/Algorithm.h
@@ -8,9 +8,12 @@ public:
 	double GetPiNumber() const;
 private:
 	static bool IsPointInCircle(float x, float y);
+	void SingleThreadAlgorithm();
+	void MultiThreadAlgorithm();
 	double m_pi;
 	static size_t m_pointsInCircleCount;
-	size_t m_iterationsCount; 
+	size_t m_iterationCount; 
 	size_t m_threadCount;
+	static DWORD WINAPI Algorithm::CalculatePointsInCircle(LPVOID data);
 };
 
