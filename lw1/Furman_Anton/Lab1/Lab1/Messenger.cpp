38174dcb8440f716c1447cf6315324512a3af8f9
Anton <vetronog@yandex.ru>
2018-06-13 19:52:03 +0300

    Исправлена ошибка при некорректности вргументов программы

--- lw1/Furman_Anton/Lab1/Lab1/Messenger.cpp
+++ lw1/Furman_Anton/Lab1/Lab1/Messenger.cpp
@@ -5,7 +5,7 @@ static const std::string ITERATION_COUNT_MESSAGE = "<iteration_count> - positive
 static const std::string THREAD_COUNT_MESSAGE = "<thread_count> - value in range:" + std::to_string(MIN_THREAD_COUNT) + " - " + std::to_string(MAX_THREAD_COUNT) + ")";
 static const std::string ARGUMENT_MESSAGE = ITERATION_COUNT_MESSAGE + THREAD_COUNT_MESSAGE + "\n";
 static const std::string HELP_MESSAGE = "Use Lab1.exe <iteration_count> <thread_count>" + ARGUMENT_MESSAGE + "/n";
-static const std::string ERROR_MESSAGE = "Invalid arguments \n" "Use Lab1.exe --help for help\n";
+static const std::string ERROR_MESSAGE = "Invalid arguments \n" "Use Lab1.exe --h for help\n";
 
 Messenger::Messenger()
 {
